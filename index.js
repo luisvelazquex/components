ed19e86faf7114b43984e581bf50f4862f9537cf
luisvelazquex <lvelazquez@tvpage.com>
2018-06-21 18:36:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import SideBar from "./components/Sidebar/lib/";
 import HamburgerMenu from "./components/HamburgerMenu/lib/";
-export const SideBar;
-export const HamburgerMenu;
+export { SideBar as SideBar};
+export { HamburgerMenu as HamburgerMenu };
 export default {
-    Sidebar,
-    HamburgerMenu
+    SideBar: SideBar,
+    HamburgerMenu: HamburgerMenu
 };
